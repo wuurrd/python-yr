65ee0c14c461327483f5dcfa5b92b041c0d2e3ec
Alexander Hansen <alexander.l.hansen@gmail.com>
2013-05-22 20:49:30 +0200

    First improvements

--- Yr/examples/client.py
+++ Yr/examples/client.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
-from ..libweather import Yr as Weather
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from libweather import Yr as Weather
 
-weather = Weather("Skien")
-print(weather.return_place())
+temperature = Weather("Norge/Oslo/Oslo/Oslo").get_temperature()
+print(temperature)
--- Yr/libweather.py
+++ Yr/libweather.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python
-api_url = ("http://www.yr.no/sted/Norge/Telemark/Skien/Skien/varsel_time_for_time.xml")
+import urllib2 as urllib
+
+api_url = ("http://www.yr.no/sted/")
+
+class Connect:
+    def __init__(self, location):
+        self.location = (location)
+        self.url = (api_url+"%s") % (self.location)
+
+    def read(self):
+        import xml.etree.cElementTree as et
+        req = urllib.Request(self.url, None, {'user-agent':'yr/wckd'})
+        opener = (urllib.build_opener())
+        f = (opener.open(req))
+        read = (f.read())
+        out = et.fromstring(read)
+        return out
 
 class Yr:
-    def __init__(self, place):
-        self.place = place
-    
+    def __init__(self, location):
+        self.location = (location)
+
     def return_place(self):
-        return(self.place)
+        return(self.location)
+
+    def get_temperature(self):
+        location = (self.location+"/forecast.xml")
+        get = Connect(location).read()
+        for temperature in get[5].iter('temperature'):
+            return temperature.attrib
+        return forecast
