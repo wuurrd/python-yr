f30b615a124537093281cd69f39501722177a215
Alexander Hansen <alexander.l.hansen@gmail.com>
2013-05-23 15:45:33 +0200

    First implementation for finding places in the json-file - not finished

--- Yr/examples/client.py
+++ Yr/examples/client.py
@@ -6,9 +6,14 @@ import os.path
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from libweather import Yr as Weather
+from libweather import place2url
 
-place = Weather('Norge/Vestfold/Tønsberg/Tønsberg')
+place = Weather('Norge/Østfold/Halden/Sponvika')
 
 temperature = place.get_temperature()
+
+finder = place2url('Oslo')
+for i in finder.find():
+    print i
 
 print temperature['value'], temperature['unit']
--- Yr/libweather.py
+++ Yr/libweather.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib2 as urllib
 
 api_url = ("http://www.yr.no/sted/")
+
+class place2url:
+    def __init__(self, location):
+        self.location = (location)
+
+    def find(self):
+        import json
+        json_file = open('places_norway.json', 'r')
+        data = json.load(json_file)
+        return data
 
 class Connect:
     def __init__(self, location):
@@ -10,12 +19,13 @@ class Connect:
         self.url = (api_url+self.location)
 
     def read(self):
+        import urllib2 as urllib
         import xml.etree.cElementTree as et
         req = urllib.Request(self.url, None, {'user-agent':'yr/wckd'})
         opener = (urllib.build_opener())
         f = (opener.open(req))
-        read = (f.read())
-        out = et.fromstring(read)
+        r = (f.read())
+        out = et.fromstring(r)
         return out
 
 class Yr:
@@ -26,10 +36,10 @@ class Yr:
         return(self.location)
 
     def get_temperature(self):
-	"""
-	Get temperature from yr and return it.
-	Returns a dict with 'value' and 'unit'.
-	"""
+        """
+        Get temperature from yr and return it.
+        Returns a dict with 'value' and 'unit'.
+        """
         xmlFile = ("/varsel.xml")
         location = (self.location+xmlFile)
         get = Connect(location).read()
