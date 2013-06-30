309791f4c3e31ed1af42863cc42d53b66868dd20
Alexander Hansen <alexander.l.hansen@gmail.com>
2013-06-30 16:54:22 +0200

    Cache implemented, closes #1

--- Yr/libyr.py
+++ Yr/libyr.py
@@ -11,67 +11,70 @@ class Yr:
     def temperature(self):
         """
         Get temperature from yr and return it.
-        Returns a dict with 'location', 'value' and 'unit'.
         """
-        try:
+        cache = Cache(self.location, "temperature")
+        if cache.is_cached() and cache.is_fresh():
+            return cache.read()
+        else:
             location = (Location(self.location, self.language).find())
             data = (Connect(location).read())
-            cache_test = (Cache(location).find())
-        except AttributeError:
-            pass
-        try:
             for temperature in data[5].iter('temperature'):
                 if temperature.attrib:
                     out = {
-                        'unit': temperature.attrib['unit'], 
-                        'value': temperature.attrib['value'], 
+                        'unit': temperature.attrib['unit'],
+                        'value': temperature.attrib['value'],
                         'location': self.location,
                         'timestamp': self.now.strftime("%d.%m.%Y %H:%M:%S")
                     }
-                    Cache(self.location).create(out)
+                    cache.create(out)
+                    out = cache.read()
                     return out
-                else:
-                    return None
-        except UnboundLocalError:
-            return {'unit': None, 'value': None, 'location': None, }
 
     def wind_speed(self):
         """
         Get wind speed from yr.
-        Returns a dict with 'location', 'unit', 'mps' and 'name'.
         """
-        location = (Location(self.location, self.language).find())
-        data = (Connect(location).read())
-
-        for wind in data[5].iter('windSpeed'):
-            if wind.attrib:
-                out = {
-                    'location': self.location,
-                    'mps': wind.attrib['mps'],
-                    'unit': str('mps'),
-                    'name': wind.attrib['name'],
-                    'timestamp': self.now.strftime("%d.%m.%Y %H:%M:%S")
-                }
-                return out
+        cache = Cache(self.location, "wind_speed")
+        if cache.is_cached() and cache.is_fresh():
+            return cache.read()
+        else:
+            location = (Location(self.location, self.language).find())
+            data = (Connect(location).read())
+            for wind in data[5].iter('windSpeed'):
+                if wind.attrib:
+                    out = {
+                        'location': self.location,
+                        'mps': wind.attrib['mps'],
+                        'unit': str('mps'),
+                        'name': wind.attrib['name'],
+                        'timestamp': self.now.strftime("%d.%m.%Y %H:%M:%S")
+                    }
+                    cache.create(out)
+                    out = cache.read()
+                    return out
 
     def wind_direction(self):
         """
         Get wind direction from yr.
-        Returns a dict with 'location', 'deg', 'code' and 'name'.
         """
-        location = Location(self.location, self.language).find()
-        data = Connect(location).read()
-
-        for wind in data[5].iter('windDirection'):
-            if wind.attrib:
-                out = {
-                    'location': self.location,
-                    'deg': wind.attrib['deg'],
-                    'code': wind.attrib['code'],
-                    'name': wind.attrib['name'],
-                    'timestamp': self.now.strftime("%d.%m.%Y %H:%M:%S")
-                }
-                return out
+        cache = Cache(self.location, "wind_direction")
+        if cache.is_cached() and cache.is_fresh():
+            return cache.read()
+        else:
+            location = Location(self.location, self.language).find()
+            data = Connect(location).read()
+            for wind in data[5].iter('windDirection'):
+                if wind.attrib:
+                    out = {
+                        'location': self.location,
+                        'deg': wind.attrib['deg'],
+                        'code': wind.attrib['code'],
+                        'name': wind.attrib['name'],
+                        'timestamp': self.now.strftime("%d.%m.%Y %H:%M:%S")
+                    }
+                    cache.create(out)
+                    out = cache.read()
+                    return out
 
 class Location:
     """
@@ -120,26 +123,35 @@ class Connect:
         return out
 
 class Cache:
-    def __init__(self, location):
-        import os
+    def __init__(self, location, cf):
         self.location = (location)
         self.cache_dir = ("/tmp/python-yr/")
+        self.cache_file = (self.cache_dir+self.location+"."+cf)
 
         if not os.path.exists(self.cache_dir):
             os.makedirs(self.cache_dir)
 
     def create(self, data):
-        print "DEBUG:", json.dumps(data)
-        if not os.path.isfile(self.cache_dir+self.location):
-            print self.cache_dir+self.location
-            cjf = open(self.cache_dir+self.location)
-            cfj.write(data)
-            cfj.close()
-        pass
-    def destroy(self):
-        pass
-    def find(self):
-        """Find the file and timestamp. 
-        Update cache if timestamp is 10 minutes or older.
-        """
-        pass
+        cf = open(self.cache_file, "w")
+        cf.write(json.dumps(data))
+        cf.close()
+
+    def is_cached(self):
+        if os.path.isfile(self.cache_file):
+            return True
+        else:
+            return False
+
+    def is_fresh(self):
+        cf = open(self.cache_file).read()
+        cfjs = json.loads(cf)
+        timestamp = datetime.datetime.strptime(cfjs['timestamp'], "%d.%m.%Y %H:%M:%S")
+        out = False
+        if datetime.datetime.now() - timestamp <= datetime.timedelta(minutes = 10):
+            out = True
+        return out
+
+    def read(self):
+        cf = open(self.cache_file).read()
+        out = json.loads(cf)
+        return out
