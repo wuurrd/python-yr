9a1f142a3d380be02c4e85df16fc68c0d14f1ccd
Alexander Hansen <alexander.l.hansen@gmail.com>
2013-07-02 13:19:46 +0000

    Cleanup. Remove unused debug-statements and imports

--- Yr/utils.py
+++ Yr/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os, errno, json, datetime, hashlib
+import os, errno, datetime, hashlib
 import xml.etree.cElementTree as et
 import urllib2 as urllib
 import unicodecsv
@@ -45,15 +45,13 @@ class Connect:
     def read(self):
         cache = Cache(self.loc_hash, "varsel")
         if cache.exists() and cache.is_fresh():
-            #print "DEBUG: read saying cache exists"
             return (cache.read())
-        else:
-            #print "DEBUG: no cache"
-            req = (urllib.Request(self.url, None, {'user-agent':'yr/wckd'}))
-            opener = (urllib.build_opener())
-            f = (opener.open(req).read())
-            cache.write(f)
-            return (cache.read())
+
+        req = (urllib.Request(self.url, None, {'user-agent':'yr/wckd'}))
+        opener = (urllib.build_opener())
+        f = (opener.open(req).read())
+        cache.write(f)
+        return (cache.read())
 
 class Cache:
     def __init__(self, location, cf):
@@ -66,7 +64,6 @@ class Cache:
 
     def write(self, data):
         cf = open(self.cache_file, "w")
-        #print "DEBUG: cache write"
         cf.write(data)
         cf.close()
 
@@ -75,19 +72,16 @@ class Cache:
         out = False
         if datetime.datetime.now() - datetime.datetime.fromtimestamp(modified) <= datetime.timedelta(minutes = 10):
             out = True
-        #print "DEBUG: cache is fresh:", out
         return out
 
     def exists(self):
         if os.path.isfile(self.cache_file):
-            #print "DEBUG: cache exists"
             return True
         else:
             return False
 
     def read(self):
         cf = open(self.cache_file).read()
-        #print "DEBUG: cache is read"
         return cf
 
     def remove(self):
