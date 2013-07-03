927607aaf0ae137d70d585d90250253d51e80559
Alexander Hansen <alexander.l.hansen@gmail.com>
2013-07-03 10:36:14 +0200

    Bumped version on setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-yr',
-      version='1.0-dev',
+      version='1.1-dev',
       description='Get the forecast from the norwegian wheather service yr.no in python',
       author='Alexander Hansen',
       author_email='alexander.l.hansen@gmail.com',
