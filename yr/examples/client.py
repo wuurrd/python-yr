441bf467ef81d8ee968bc6e8a892b31a526e174c
idxxx23 <idxxx23@gmail.com>
2014-02-20 15:00:05 +0100

    remove import json from client.py

--- yr/examples/client.py
+++ yr/examples/client.py
@@ -4,7 +4,6 @@ import os.path, sys
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(parent_directory)
 from libyr import Yr
-import json
 
 weather = Yr('Norge/Telemark/Skien/Skien')
 #weather = Yr('Czech_Republic/Prague/Prague')
