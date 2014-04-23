ce4a8f34f39ad30c7d60576e030a74bc25dcf9ae
Alexander Hansen <wckd@users.noreply.github.com>
2014-04-23 08:44:39 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-yr',
-      version='1.2.1-dev',
+      version='1.2.2-dev',
       description='Get the forecast from the norwegian wheather service yr.no in python',
       author='wckd @ github, idxxx23 @ github',
       author_email='alexander.l.hansen@gmail.com',
